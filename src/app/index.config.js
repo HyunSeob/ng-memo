e349abf22fa8da1a6a696c2785291bb8709d141e
HyunSeob <leehs1017@naver.com>
2016-03-23 20:03:19 +0900

    HTML5 Mode

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,7 +1,8 @@
-export function config ($logProvider, toastrConfig) {
+export function config ($logProvider, $locationProvider, toastrConfig) {
   'ngInject';
   // Enable log
   $logProvider.debugEnabled(true);
+  $locationProvider.html5Mode(true);
   // Set options third-party lib
   toastrConfig.timeOut = 2000;
   toastrConfig.positionClass = 'toast-top-right';
